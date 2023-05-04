bca117ceb9fb883e2136aa153f44c9cbb6fd0f5f
giuliabruno05 <giuliabruno48@gmail.com>
2023-05-04 11:16:34 +0200

    esercizo j-snack

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//Il software deve chiedere per 5 volte all’utente di inserire un numero.
+ //Il programma stampa la somma di tutti i numeri inseriti.
+
+ //dichiaro le costanti
+ const numeriUtente = [];
+ const container = document.getElementById("container");
+
+ //dichiaro la variabile 
+let sommaNumeri = 0 ;
+console.log(sommaNumeri);
+
+ for (let i = 1; i <=5; i++){
+    let numeriInseriti =  parseInt(prompt("numeri da inserire"));
+
+    numeriUtente.push(numeriInseriti)
+    console.log(numeriUtente);
+
+    sommaNumeri= sommaNumeri + numeriInseriti;
+
+}
+
+container.innerHTML = `La somma dei numeri è ${sommaNumeri}`
+console.log(sommaNumeri);
+ 
